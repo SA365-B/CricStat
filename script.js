7b2c19c347fabf5b27fc430d67d0ea453d0f515e
SA365-B <79658929+SA365-B@users.noreply.github.com>
2023-07-15 19:20:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=1c312637-f30f-4825-9470-0a12d81832e1&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            //add your api key from cricketdata.org
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
